863069a2a74cda07be16b12a48bf84b3c49bf64b
Mahmoud Khaled <37103979+mahmoudxkhaled@users.noreply.github.com>
2026-03-11 21:42:09 +0200

    This screen is what will appear in for a wrong route address. We need to have something related to the ERP system:

--- src/app/core/components/notfound/notfound.component.ts
+++ src/app/core/components/notfound/notfound.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-	templateUrl: './notfound.component.html'
+	templateUrl: './notfound.component.html',
+	styleUrls: ['./notfound.component.scss']
 })
-export class NotfoundComponent { }
+export class NotfoundComponent {
+
+	constructor(
+		private location: Location,
+		public translate: TranslateService
+	) { }
+
+	get isRtl(): boolean {
+		return this.translate.currentLang === 'ar';
+	}
+
+	goBack(): void {
+		this.location.back();
+	}
+}
 
 
--- src/app/core/components/notfound/notfound.module.ts
+++ src/app/core/components/notfound/notfound.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { NotfoundRoutingModule } from './notfound-routing.module';
 import { NotfoundComponent } from './notfound.component';
 import { ButtonModule } from 'primeng/button';
@@ -8,6 +10,8 @@ import { RippleModule } from 'primeng/ripple';
 @NgModule({
     imports: [
         CommonModule,
+        RouterModule,
+        TranslateModule,
         NotfoundRoutingModule,
         ButtonModule,
         RippleModule
