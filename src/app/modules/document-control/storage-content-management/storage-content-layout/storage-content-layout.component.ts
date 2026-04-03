c7c8dfb49a0316afed53d2083935c560af253696
Mahmoud Khaled <37103979+mahmoudxkhaled@users.noreply.github.com>
2026-04-03 19:07:59 +0200

    up

--- src/app/modules/document-control/storage-content-management/storage-content-layout/storage-content-layout.component.ts
+++ src/app/modules/document-control/storage-content-management/storage-content-layout/storage-content-layout.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 
-/**
- * Wrapper for Storage & Content Management routes.
- * Provides a router-outlet for: landing (4 cards), company-storage, shared-files, DCS, EDMS.
- */
+
 @Component({
     selector: 'app-storage-content-layout',
     template: `
@@ -11,4 +8,4 @@ import { Component } from '@angular/core';
         <p-toast></p-toast>
     `
 })
-export class StorageContentLayoutComponent {}
+export class StorageContentLayoutComponent { }
